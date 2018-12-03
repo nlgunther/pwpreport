b647803d99ecebc9b73f2072cb6e490c41d051c4
Nicholas Gunther <nlgunther@gmail.com>
2018-12-03 07:28:02 -0800

    Changes to comments, urls and views, including corrections to decimal places in PWP report for 'small' sectors.

--- report/urls.py
+++ report/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.snpsector, name='snpsector.html'),
+    path(r'test',views.snpsectorwip, name='snpsectorswip.html'),
 ]
 
--- report/views.py
+++ report/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def snpsector(request):
     return render(request, 'report/snpsector.html', {})
+    
+def snpsectorwip(request):
+    return render(request,'report/snspsectorwip.html',{})
